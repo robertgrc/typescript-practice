3b25b3a33c9a6aa687c982ea15045c2bfb65d838
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-08-25 17:58:53 -0400

    objetos e interfaces 4 homeworktypes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
-import './topics/3-functions'
+// import './topics/3-functions'
+import './topics/4-homework-types'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
