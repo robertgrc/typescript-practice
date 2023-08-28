5534b0db2d2834479da8c5d4f4034c6d55436222
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-08-28 17:10:15 -0400

    Constructor de una Clase

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -2,15 +2,22 @@
 
 export class Person {
 
-    public name?: string;
-    public address?: string;
+   //* primera forma de hacer 
+    // public name?: string;
+    // public address?: string;
 
-    constructor(){
-        this.name = 'Fernando';
-        this.address = 'New York'
-    }
+    // constructor(name: string, address: string){
+    //     this.name = name;
+    //     this.address = address;
+    // }
+  
+   //* segunda forma mas usual es lo mismo que el codigo de arriba
+    constructor(
+        public name: string, 
+        public address: string = 'No Address'
+    ){}
 }
 
-const ironman = new Person();
+const ironman = new Person( 'johnwick', 'bolivia' );
 console.log(ironman)
 console.log('ironman.name', ironman.name)
