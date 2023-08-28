7665bc9845fed3a0b67acbf610ab1f78cc34a9fb
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-08-28 17:46:21 -0400

    Priorizar composicion sobre herencia

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -1,4 +1,5 @@
-
+//*--- Aqui tenemos un ejemplo donde la clase Hero Extiende de clase Person 
+//*Donde ponemos el Super se tiene que poner las propiedades del padre en este caso tenemos que poner en super( name y address?) porque  Hero esta extendiendo de Person linea22  entonces el padre de Hero seria Person y se accede mediante Super()
 
 export class Person {
 
@@ -18,18 +19,30 @@ export class Person {
     ){}
 }
 
-export class Hero extends Person {
+// export class Hero extends Person {
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string,
+//     ) {
+//         super('batman', 'ciudad gotica777')
+//     }
+// }
+export class Hero {
+
+    //public person: Person;
+
     constructor(
         public alterEgo: string,
         public age: number,
         public realName: string,
+        public person: Person,
     ) {
-        super('batman', 'ciudad gotica777')
+        this.person = new Person(realName)
     }
 }
 
 
-// const jhonwick = new Person( 'johnwick', 'bolivia' );
-const batman = new Hero( 'bruce wayne', 38, 'Bruno Diaz' );
+const infoBatman = new Person('Batman', 'Ciudad Gotica');
+const batman = new Hero( 'bruce wayne', 38, 'Bruno Diaz', infoBatman );
 console.log(batman)
-console.log('batman.name', batman.name)
